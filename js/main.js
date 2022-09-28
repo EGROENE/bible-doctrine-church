7738cda1cc5dfaf1c0bbae85f3e4ce3897def770
Ethan Groene <ethangroene@gmail.com>
2022-09-27 21:03:49 -0500

    Add functionality to pop deacons info w/ JS

--- js/main.js
+++ js/main.js
@@ -11,3 +11,21 @@ const popHomepageLinkOfDocsInPages = () => {
     // Convert header to flex (from grid), so items display in correct order:
     universalPgHeader.style.display = 'flex';
 }
+
+// Functionality to populate .deacons-container on about.html:
+const deaconsInfo = [
+    { deaconNameTitle: "Phillip Rewerts, President", deaconPhone: "616-560-3592" },
+    { deaconNameTitle: "Tom Kilebrew, Vice President-Sunday School", deaconPhone: "616-498-4861" },
+    { deaconNameTitle: "Mark Verstraete, Treasurer", deaconPhone: "616-402-2769" },
+    { deaconNameTitle: "Ken Hildebrand, Secretary", deaconPhone: "231-865-6711" },
+    { deaconNameTitle: "Ross Regua, Communion", deaconPhone: "310-507-4539" }
+]
+const deaconsContainer = document.getElementById('deacons-container');
+const popDeaconsSection = () => {
+    for (deacon of deaconsInfo) {
+        deaconsContainer.innerHTML += "<div>"
+        + "<header>" + deacon.deaconNameTitle + "</header>"
+        + "<p>" + deacon.deaconPhone + "</p>"
+        + "</div>"
+    }
+}
