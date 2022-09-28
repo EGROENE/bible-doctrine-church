46b3cb18b8e3992a45789ef49bc255c274ab8a9a
Ethan Groene <ethangroene@gmail.com>
2022-09-28 00:46:47 -0500

    Add func to pop homepage link of pg header in index.html
    Done for styling purposes, so it matches MR design of docs in pages/

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 // START JS FOR DEFAULT ITEMS
+// Populate homepage link in header of index.html:
+const universalPgHeaderHomepage = document.getElementById('universal-pg-header');
+const popHomepageLinkOfHomepage = () => {
+    universalPgHeaderHomepage.innerHTML += "<a id='homepage-link' href='./index.html'>"
+    + "<img src='./assets/images/main-logo-2.png' alt='bdc-logo' style='width: 21.07%'>"
+    + "<div>"
+    + "<h1>Bible Doctrine Church</h1>"
+    + "<h2>of West Michigan</h2>"
+    + "</div>"
+    + "</a>"
+    // Convert header to flex (from grid), so items display in correct order:
+    universalPgHeaderHomepage.style.display = 'flex';
+}
+
 // Populate homepage link in header of all docs in pages/
-const universalPgHeader = document.getElementById('universal-pg-header');
+const universalPgHeaderPages = document.getElementById('universal-pg-header');
 const popHomepageLinkOfDocsInPages = () => {
-    universalPgHeader.innerHTML += "<a id='homepage-link' href='../index.html'>"
+    universalPgHeaderPages.innerHTML += "<a id='homepage-link' href='../index.html'>"
     + "<img src='../assets/images/main-logo-2.png' alt='bdc-logo' style='width: 21.07%'>"
     + "<div>"
     + "<h1>Bible Doctrine Church</h1>"
@@ -10,7 +24,7 @@ const popHomepageLinkOfDocsInPages = () => {
     + "</div>"
     + "</a>"
     // Convert header to flex (from grid), so items display in correct order:
-    universalPgHeader.style.display = 'flex';
+    universalPgHeaderPages.style.display = 'flex';
 }
 
 // Functionality to populate footer info on all pages:
